98035ca19244f8c29f4f1e414a0b97affb5c1ba7
Joey Harrison <joey@joeyh.dev>
2023-02-10 20:04:10 +0000

    formatting

--- uwcsvote/settings/dev.py
+++ uwcsvote/settings/dev.py
@@ -6,21 +6,21 @@ from .base import *
 DEBUG = True
 
 ALLOWED_HOSTS += [
-    'localhost',
-    '127.0.0.1',
+    "localhost",
+    "127.0.0.1",
 ]
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'l33th4x0rs'
+SECRET_KEY = "l33th4x0rs"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'uwcs_vote',
-        'USER': 'uwcs_vote',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '5432',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "uwcs_vote",
+        "USER": "uwcs_vote",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
